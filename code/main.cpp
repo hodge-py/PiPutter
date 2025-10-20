a00e3a0f4dddd2c9aa7b884512076a91060b5ca5
hodge-py <105604814+hodge-py@users.noreply.github.com>
2025-10-19 18:51:57 -0600

    c

--- code/main.cpp
+++ code/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
+void testFunction() {
+    // This is a test function to demonstrate C++17 features
+    std::cout << "this is my test for this function" << std::endl;
+
+}
+
+
 int main() {
     std::cout << "Hello, C++!" << std::endl;
+    testFunction();
     return 0;
 }
+
 
